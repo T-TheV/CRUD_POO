d0952c8bcd543015cf797cfab170b3dc9c3fa456
T-thev <djardim322@gmail.com>
2025-01-15 17:24:32 -0300

    ADD: Conecção com banco ao crud

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ const { Curso } = require("./src/models/Curso");
 
 const alunos = new AlunoController()
 
+alunos.adicionarAluno('testandoaaa','fulano@email.com','84899798','5555555','tec em desenvolvimento');
 alunos.listarAluno();
